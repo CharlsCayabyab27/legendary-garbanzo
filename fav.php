71bc97d0b3408fb757106a739ee12e879a62a8b4
Charls Brent Cayabyab <117609343+CharlsCayabyab27@users.noreply.github.com>
2023-03-03 07:01:47 +0800

    web 2 done

---
+++ fav.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Your Favorite</title></head>
+<body>
+    Thank you for your selection <?php echo $_POST["username"]; ?><br>
+   
+    
+    You really enjoyed <?php echo $_POST["dish"]; ?><br>
+
+
+    especially with a nice <?php echo $_POST["color"]; ?>
+    wine 
+</body>
+</html>
