0640a95f68d3992d48d0e70d21cbe8c0b9f9668d
FranzDev <68122334+Franzvalonda@users.noreply.github.com>
2023-03-02 22:46:57 +0800

    web5 Done

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,27 @@
+
 <?php
 
-?>
+session_start();
 
+
+if (isset($_GET['reset'])) {
+    session_destroy();
+    header('Location: index.php');
+} else {
+
+    $num_visits = $_SESSION['num_visits'] ?? 1;
+    if ($num_visits >= 5 && $num_visits <= 9) {
+
+        echo "Thank you for visiting the page a lot.</br>";
+    } else if ($num_visits >= 10) {
+
+        header('Location: congratulations.php');
+    }
+
+    $_SESSION['num_visits'] = $num_visits + 1;
+}
+
+?>
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
